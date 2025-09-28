b8e4c8db57205e40049310d000856c067f425258
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-09-29 00:01:05 +0400

    Added a resize function to resize the grid with maximum of 100x100

--- projects/etch-a-sketch/script.js
+++ projects/etch-a-sketch/script.js
@@ -1,23 +1,45 @@
-const gridContainer = document.getElementById("gridContainer");
+let gridContainer = document.getElementById("gridContainer");
 
 // Create divs in container so we can have a 16x16 grid.
-for (let i = 1; i <= 16; i++) {
-  let column = document.createElement("div");
-  column.className = "column";
-  column.id = `column${i}`;
-  // add hover to draw for columns
-  column.onmouseover = () => {
-    column.style.backgroundColor = "black";
-  };
-  gridContainer.appendChild(column);
-  for (let j = 1; j <= 16; j++) {
-    let row = document.createElement("div");
-    row.className = "row";
-    row.id = `row${j}`;
-    // add hover to draw for rows
-    row.onmouseover = () => {
-      row.style.backgroundColor = "black";
+function createGrid(size) {
+  for (let i = 1; i <= size; i++) {
+    let column = document.createElement("div");
+    column.className = "column";
+    column.id = `column${i}`;
+    // add hover to draw for columns
+    column.onmouseover = () => {
+      column.style.backgroundColor = "black";
     };
-    column.appendChild(row);
+    gridContainer.appendChild(column);
+    for (let j = 1; j <= size; j++) {
+      let row = document.createElement("div");
+      row.className = "row";
+      row.id = `row${j}`;
+      // add hover to draw for rows
+      row.onmouseover = () => {
+        row.style.backgroundColor = "black";
+      };
+      column.appendChild(row);
+    }
   }
 }
+
+// Load the initial grid
+createGrid(16);
+
+const resizeButton = document.getElementById("resizeButton");
+
+function deleteGrid() {
+  // Clears the innerHTML which deletes every single row and column, effectively deleting the entirety of the grid.
+  gridContainer.innerHTML = "";
+}
+
+resizeButton.onclick = () => {
+  // the max size is 100 so it isn't too big that it'll freeze or cause size issues
+  let size = prompt("Enter a size (MAX 100): ");
+  while (size > 100) {
+    size = prompt("Enter a size (MAX 100): ");
+  }
+  deleteGrid();
+  createGrid(size);
+};
