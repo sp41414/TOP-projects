951ce19a9743a3a927cd8dd9305a0f1ae669aa64
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-10-09 18:17:19 +0400

    Finished tic-tac-toe grid

--- projects/javascript/tic-tac-toe/script.js
+++ projects/javascript/tic-tac-toe/script.js
@@ -22,6 +22,15 @@ const Player = (marker) => {
 }
 const gameContainer = document.getElementById("game-container")
 
+function initializeGameBoard() {
+	for (i = 1; i <= 9; i++) {
+		let cell = document.createElement("div")
+		cell.className = "cell"
+		cell.id = `${i}`
+		gameContainer.appendChild(cell)
+	}
+}
+
 let player = Player(1);
 
 const choiceContainer = document.getElementById("choice-container");
@@ -31,10 +40,26 @@ const markerOSelection = document.getElementById("marker-o");
 markerXSelection.addEventListener("click", () => {
 	player = Player(1);
 	choiceContainer.style.display = "none";	
-	gameContainer.style.display = "block";
+	initializeGameBoard();
+	gameContainer.style.display = "grid";
+	gameContainer.style.cssText = `
+		display: grid; 
+		grid-template-rows: 1fr 1fr 1fr;
+		grid-template-columns: 1fr 1fr 1fr;
+		width: 50%;
+		height: 50%;
+	`
 })
 markerOSelection.addEventListener("click", () => {
 	player = Player(2);
 	choiceContainer.style.display = "none";
-	gameContainer.style.display = "block";
+	initializeGameBoard();
+	gameContainer.style.display = "grid";
+		gameContainer.style.cssText = `
+		display: grid; 
+		grid-template-rows: 1fr 1fr 1fr;
+		grid-template-columns: 1fr 1fr 1fr;
+		width: 50%;
+		height: 50%;
+	`
 })
