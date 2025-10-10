5ee70d1da13a8c0287e09250e54894160e208a7a
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-10-10 18:10:35 +0400

    Changed Book constructor to class

--- projects/javascript/library/script.js
+++ projects/javascript/library/script.js
@@ -1,18 +1,20 @@
 const library = [];
 
-function Book(name, author, releaseDate) {
-  this.id = crypto.randomUUID();
-  this.name = name;
-  this.author = author;
-  this.releaseDate = releaseDate;
-  this.isRead = "Unread";
-  this.read = () => {
-    if (this.isRead === "Read") {
-      this.isRead = "Unread";
-    } else {
-      this.isRead = "Read";
-    }
-  };
+class Book {
+	constructor(name, author, releaseDate) {
+		this.id = crypto.randomUUID();
+		this.name = name;
+		this.author = author;
+		this.releaseDate = releaseDate;
+		this.isRead = "Unread";
+	}
+	read() {
+		if (this.isRead === "Unread") {
+			this.isRead = "Read";
+		} else {
+			this.isRead = "Read";
+		}
+	}
 }
 
 function addBookToLibrary(book) {
@@ -22,61 +24,61 @@ function addBookToLibrary(book) {
 function displayBooks(library) {
   const booksContainer = document.getElementById("books");
   booksContainer.innerHTML = "";
-  for (book in library) {
-    const bookContainer = document.createElement("div");
-    bookContainer.className = "book-container";
-    bookContainer.id = `${library[book].id}`;
+	for (let book in library) {
+		const bookContainer = document.createElement("div");
+		bookContainer.className = "book-container";
+		bookContainer.id = `${library[book].id}`;
 
-    const bookName = document.createElement("p");
-    bookName.textContent = library[book].name;
-    bookName.className = "book-name";
+		const bookName = document.createElement("p");
+		bookName.textContent = library[book].name;
+		bookName.className = "book-name";
 
-    const bookAuthor = document.createElement("p");
-    bookAuthor.textContent = library[book].author;
-    bookAuthor.className = "book-author";
+		const bookAuthor = document.createElement("p");
+		bookAuthor.textContent = library[book].author;
+		bookAuthor.className = "book-author";
 
-    const releaseDate = document.createElement("p");
-    releaseDate.className = "book-release-date";
-    releaseDate.textContent = library[book].releaseDate;
+		const releaseDate = document.createElement("p");
+		releaseDate.className = "book-release-date";
+		releaseDate.textContent = library[book].releaseDate;
 
-    var isReadButton = document.createElement("button");
-    isReadButton.className = "is-read-button";
-    isReadButton.textContent = "Read";
-    isReadButton.addEventListener("click", (button) => {
-      const id = button.target.parentElement.id;
-      const book = library.find((b) => b.id === id);
-      const readText =
-        button.target.parentElement.querySelector(".unread, .read");
+		let isReadButton = document.createElement("button");
+		isReadButton.className = "is-read-button";
+		isReadButton.textContent = "Read";
+		isReadButton.addEventListener("click", (button) => {
+			const id = button.target.parentElement.id;
+			const book = library.find((b) => b.id === id);
+			const readText =
+				button.target.parentElement.querySelector(".unread, .read");
 
-      if (book) {
-        book.read();
-        readText.textContent = book.isRead;
-        readText.className = book.isRead === "Read" ? "read" : "unread";
-      }
-    });
+			if (book) {
+				book.read();
+				readText.textContent = book.isRead;
+				readText.className = book.isRead === "Read" ? "read" : "unread";
+			}
+		});
 
-    isReadText = document.createElement("p");
-    isReadText.className = "unread";
+		let isReadText = document.createElement("p");
+		isReadText.className = "unread";
 
-    isReadText.textContent = library[book].isRead;
+		isReadText.textContent = library[book].isRead;
 
-    removeButton = document.createElement("button");
-    removeButton.className = "remove-button";
-    removeButton.textContent = "Remove Book";
+		let removeButton = document.createElement("button");
+		removeButton.className = "remove-button";
+		removeButton.textContent = "Remove Book";
 
-    booksContainer.appendChild(bookContainer);
-    bookContainer.appendChild(bookName);
-    bookContainer.appendChild(bookAuthor);
-    bookContainer.appendChild(releaseDate);
-    bookContainer.appendChild(isReadButton);
-    bookContainer.appendChild(removeButton);
-    bookContainer.appendChild(isReadText);
+		booksContainer.appendChild(bookContainer);
+		bookContainer.appendChild(bookName);
+		bookContainer.appendChild(bookAuthor);
+		bookContainer.appendChild(releaseDate);
+		bookContainer.appendChild(isReadButton);
+		bookContainer.appendChild(removeButton);
+		bookContainer.appendChild(isReadText);
 
-    removeButton.addEventListener("click", () => {
-      booksContainer.removeChild(bookContainer);
-      library.splice(library.indexOf(book), 1);
-    });
-  }
+		removeButton.addEventListener("click", () => {
+			booksContainer.removeChild(bookContainer);
+			library.splice(library.indexOf(book), 1);
+		});
+	}
 }
 
 const newBookButton = document.getElementById("add-book");
