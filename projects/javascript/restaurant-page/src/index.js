f51597e91bb113804dced6763fae5e29b0da45e2
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-10-11 18:18:15 +0400

    Basic styling

--- projects/javascript/restaurant-page/src/index.js
+++ projects/javascript/restaurant-page/src/index.js
@@ -3,6 +3,7 @@ import Home from "./home.js"
 
 const content = document.getElementById("content");
 
+content.innerHTML = "";
 Home();
 
 document.getElementById("home").addEventListener("click", () => {
