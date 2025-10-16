65bbb2e0cfcdbbdeeec3c0f6c760ad7244f24e1d
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-10-16 21:56:02 +0400

    Added html to view practice lessons

--- practice/dropdown-practice/src/index.js
+++ practice/dropdown-practice/src/index.js
@@ -1,29 +1,3 @@
-export function dropdownElement(contentArray) {
-  const dropdown = document.createElement("div");
-  dropdown.style.cssText = `
-		background-color: black;
-		color: white;
-		width: max-content;
-		padding: 1em;
-	`;
-  dropdown.textContent = "Dropdown";
-  document.body.appendChild(dropdown);
-  contentArray.forEach((element) => {
-    let option = document.createElement("div");
-    option.className = "invisible";
-    option.textContent = element;
-    dropdown.appendChild(option);
-  });
-  dropdown.addEventListener("mouseover", () => {
-    const invisibleElements = document.getElementsByClassName("invisible");
-    for (let item of invisibleElements) {
-      item.style.display = "block";
-    }
-  });
-  dropdown.addEventListener("mouseout", () => {
-    const invisibleElements = document.getElementsByClassName("invisible");
-    for (let item of invisibleElements) {
-      item.style.display = "none";
-    }
-  });
-}
+import { dropdownElement } from "./dropdown.js";
+
+dropdownElement(["Option 1", "Option 2", "Option 3"]);
