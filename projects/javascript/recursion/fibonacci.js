451f53a6c82ecdecd7c1584233d1b7164481c590
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-10-20 22:59:37 +0400

    Completed fibonacci project

--- projects/javascript/recursion/fibonacci.js
+++ projects/javascript/recursion/fibonacci.js
@@ -22,22 +22,3 @@ function fibsRec(number) {
 
 console.log(fibs(8))
 console.log(fibsRec(8));
-
-
-/* 
-    how merge sort works, so i dont forget:
-
-    It takes an array, splits in half, half, half, until only 1 value remains for each half.
-    it sorts that half to the other half and merges them.
-    up until the original length, with the array sorted.
-
-    Base case:
-    the array halves lengths are one.
-*/
-
-function mergeSort(array) {
-    if (array.length === 1) {
-        return []
-    }
-    
-}
