a9672c554b60bfc9b20d6ef6f12f47372ddeb0a6
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-10-22 17:59:07 +0400

    Completed binary search for fun

---
+++ practice/cs/binarySearch.js
@@ -0,0 +1,21 @@
+// Binary search O(log n)
+function search(element, array) {
+  let midpoint = Math.floor(array.length / 2);
+  let middle = array[midpoint];
+  if (element === middle) return "Found";
+  if (middle === undefined || array === undefined) return "Not found";
+
+  if (element < middle) {
+    return search(element, array.slice(0, midpoint));
+  } else {
+    return search(element, array.slice(midpoint));
+  }
+}
+
+array = [0, 1, 2, 3, 4, 7, 8];
+console.log(search(8, array)); // Found
+
+// Array method O(n)
+if (array.indexOf(8)) {
+  console.log("Found (if condition)");
+}
