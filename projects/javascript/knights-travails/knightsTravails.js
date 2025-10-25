05da6786bb954f42d96a8d5b541d6d8c5e88cf17
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-10-26 00:45:23 +0400

    Completed main logic, TODO: BFS logic

--- projects/javascript/knights-travails/knightsTravails.js
+++ projects/javascript/knights-travails/knightsTravails.js
@@ -1,3 +1,32 @@
-class Knight {
-  constructor() {}
+// Uses BFS
+function knightMoves(array1, array2) {
+  // variables
+  // list of possible moves
+  let moves = [
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2],
+  ];
+  // track the path
+  let queue = [array1, [array1]];
+  let visited = new Set();
+  visited.add(array1.toString());
+
+  while (queue.length > 0) {
+    let [current, path] = queue.shift();
+    if (current[0] === array2[0] && current[1] === array2[1]) {
+      console.log(
+        `=> You made it in ${path.length - 1} moves! Here's your path: `
+      );
+      path.forEach((element) => {
+        console.log(element);
+      });
+      return path;
+    }
+  }
 }
