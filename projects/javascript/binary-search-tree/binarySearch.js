cdab126adb91d1f0073c5b33cd14a1b208e73ebd
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-10-25 13:55:41 +0400

    finished build tree, TODO: fix lopsided tree

--- projects/javascript/binary-search-tree/binarySearch.js
+++ projects/javascript/binary-search-tree/binarySearch.js
@@ -8,11 +8,61 @@ class Node {
 
 class Tree {
   constructor(array) {
-    this.array = array;
-    this.root = this.buildTree(array);
+    this.array = this.sortArray(array);
+    this.root = this.buildTree(this.array);
   }
-  buildTree(array) {}
+  sortArray(array) {
+    if (array.length <= 1) return array;
+    let midpoint = Math.floor(array.length / 2);
+    let firstHalf = array.slice(0, midpoint);
+    let secondHalf = array.slice(midpoint);
+    let merged = [];
+    let recursedFirst = this.sortArray(firstHalf);
+    let recursedSecond = this.sortArray(secondHalf);
+    while (recursedFirst.length > 0 && recursedSecond.length > 0) {
+      // same merge sort but with duplicates filter
+      if (recursedFirst[0] === recursedSecond[0]) {
+        let value = recursedFirst.shift();
+        recursedSecond.shift();
+        merged.push(value);
+        continue;
+      }
+      let value =
+        recursedFirst[0] > recursedSecond[0]
+          ? recursedSecond.shift()
+          : recursedFirst.shift();
+      merged.push(value);
+    }
+    merged = merged.concat(recursedFirst, recursedSecond);
+    return merged;
+  }
+  buildTreeRec(array, start, end) {
+    if (start > end) return null;
+    let middle = start + Math.floor((end - start) / 2);
+    let node = new Node(array[middle]);
+    node.left = this.buildTreeRec(array, start, middle - 1);
+    node.right = this.buildTreeRec(array, middle + 1, end);
+    return node;
+  }
+  buildTree(array) {
+    return this.buildTreeRec(array, 0, array.length - 1);
+  }
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
 }
-
 let tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-console.log(tree);
+console.log(tree.prettyPrint(tree.root));
