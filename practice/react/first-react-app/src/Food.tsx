e9493af3711ed7f291d7c72df094f21e1056264e
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-11-01 14:28:47 +0400

    Updated react practice

---
+++ practice/react/first-react-app/src/Food.tsx
@@ -0,0 +1,9 @@
+export default function FavoriteFood() {
+  return (
+    <img
+      src="https://content.jwplatform.com/thumbs/84qpal4P-720.jpg"
+      alt="pasta"
+      width="400"
+    />
+  );
+}
